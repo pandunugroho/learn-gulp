cef7f4ac62c179ae6de0401105efbf644ba49ff4
pandunugroho <15pandu.nugroho@gmail.com>
2020-06-03 06:12:23 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp")
+// const imagemin = require("gulp")
+// const uglify = require("gulp")
+// const sass = require("gulp")
+// const concat = require("gulp")
+
+// function defaultTask(cb) {
+//   // place code for your default task here
+//   cb();
+// }
+
+// exports.default = defaultTask
+
+gulp.task('default', function(){
+  return console.log("Gulp is running...");
+})
