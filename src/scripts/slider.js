5a8a614017346b5a5584c9639d5c63730da682da
pandunugroho <15pandu.nugroho@gmail.com>
2020-06-03 15:09:38 +0700

    done, carousel button need improvement

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+var slideIndex = 3;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -8,10 +8,12 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("cardSlide");
-  if (n > x.length) { slideIndex = 1 }
+  if (n > x.length) { slideIndex = 3 }
   if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+    x[i].style.display = "none";
   }
-  x[slideIndex - 1].style.display = "none";
+  x[slideIndex - 1].style.display = "flex";
+  x[slideIndex - 2].style.display = "flex";
+  x[slideIndex - 3].style.display = "flex";
 }
